19edaef05d6ccf1005e593537d4549feb3696b59
sousa2323 <sousanot.23@gmail.com>
2025-09-24 20:45:17 -0300

    Estilizando os dots do indicadores do ciclo do pomodoro

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import './styles/theme.css';
 import './styles/global.css';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
 
 export function App() {
   return (
@@ -25,7 +26,7 @@ export function App() {
       <Container>
         <form className='form' action=''>
           <div className='formRow'>
-            <DefaultInput id='meuInput' type='text' />
+            <DefaultInput labelText='Tarefa' id='meuInput' type='text' placeholder='Digite algo...' />
           </div>
 
           <div className='formRow'>
@@ -33,8 +34,7 @@ export function App() {
           </div>
 
           <div className='formRow'>
-            <p>Ciclos</p>
-            <p>0 0 0 0 0 0</p>
+            <Cycles />
           </div>
 
           <div className='formRow'>
