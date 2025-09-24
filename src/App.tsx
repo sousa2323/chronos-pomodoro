2a6dac300b0ec802541dde1d58f51909fe544319
sousa2323 <sousanot.23@gmail.com>
2025-09-24 16:59:04 -0300

    Criando componente Menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+
 import './styles/theme.css';
 import './styles/global.css';
 
-import { Heading } from './components/Heading';
-import { Container } from './components/Container';
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>Logo</Heading>
+        <Logo />
       </Container>
 
       <Container>
-         <Heading>menu</Heading>
+         <Menu />
       </Container>
     </>
   );
