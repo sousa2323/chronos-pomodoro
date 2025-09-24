71f4d8eab4d45075ef3b876325d8939644d56fe8
sousa2323 <sousanot.23@gmail.com>
2025-09-24 17:53:45 -0300

    Input complexos

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import { Menu } from './components/Menu';
 
 import './styles/theme.css';
 import './styles/global.css';
-
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
 export function App() {
   return (
@@ -14,7 +15,32 @@ export function App() {
       </Container>
 
       <Container>
-         <Menu />
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput id='meuInput' type='text' />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
