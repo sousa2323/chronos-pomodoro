d65741c0b138c58ebe242e2beea34a87502521cd
sousa2323 <sousanot.23@gmail.com>
2025-10-01 20:48:46 -0300

    Criando a página Home e um template para todas as outras páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,8 @@
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
+import { Home } from './pages/Home';
 
 import './styles/theme.css';
 import './styles/global.css';
-import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayCircle } from 'lucide-react';
-import { Footer } from './components/Footer';
-
 
 export function App() {
-  return (
-    <>
-
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              labelText='Tarefa'
-              id='meuInput'
-              type='text'
-              placeholder='Digite algo...'
-            />
-          </div>
-
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-
-          <div className='formRow'>
-            <Cycles />
-          </div>
-
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircle />} />
-          </div>
-        </form>
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
