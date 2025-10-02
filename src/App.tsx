05b17e82d52b67447951e0b0f3436d11efd776cb
sousa2323 <sousanot.23@gmail.com>
2025-10-02 17:46:39 -0300

    Alteração nos templates

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { Home } from './pages/Home';
 
 import './styles/theme.css';
 import './styles/global.css';
-import { NotFound } from './pages/NotFound';
-import { AbountPomodoro } from './pages/AbountPomodoro';
+// import { NotFound } from './pages/NotFound';
+// import { AbountPomodoro } from './pages/AbountPomodoro';
 
 export function App() {
   return <Home />;
